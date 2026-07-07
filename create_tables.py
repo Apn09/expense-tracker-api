c366d63f59741292c4b1fae9adc39f3299218c52
karthi apn <karthiapn@karthis-MacBook-Air.local>
2026-07-07 22:06:44 +0530

    feat: initialize Expense Tracker API project

--- create_tables.py
+++
@@ -1,6 +0,0 @@
-from src.db.session import Base, engine
-from src.models.expense import Expense
-
-print("Creating tables...")
-Base.metadata.create_all(bind=engine)
-print("Tables created successfully!")
