80d6bb4ec790918612988a75e0c3e0a68e9fcdb6
karthi apn <karthiapn@karthis-MacBook-Air.local>
2026-07-31 07:02:18 +0530

    Clean project structure and remove temporary files

--- app/main.py
+++ app/main.py
@@ -1,3 +1,8 @@
+from app.core.config import settings
+
+print("SECRET_KEY:", settings.SECRET_KEY)
+print("ALGORITHM:", settings.ALGORITHM)
+print("TOKEN EXPIRE:", settings.ACCESS_TOKEN_EXPIRE_MINUTES)
 from fastapi import FastAPI
 from app.core.config import settings
 app = FastAPI(
--- test_config.py
+++
@@ -1,5 +0,0 @@
-from app.core.config import settings
-
-print(settings.APP_NAME)
-print(settings.DATABASE_URL)
-print(settings.REDIS_URL)
