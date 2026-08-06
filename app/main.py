0aebc5be4038322d5640a1453669a042993cccfe
karthi apn <karthiapn@karthis-MacBook-Air.local>
2026-08-06 22:01:15 +0530

    Add database health endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 
 from app.core.config import settings
 from app.api.v1.health import router as health_router
+from app.api.v1.database_health import router as database_health_router
 
 print("SECRET_KEY:", settings.SECRET_KEY)
 print("ALGORITHM:", settings.ALGORITHM)
@@ -12,6 +13,7 @@ app = FastAPI(
     version="1.0.0"
 )
 app.include_router(health_router)
+app.include_router(database_health_router)
 
 @app.get("/")
 def root():
